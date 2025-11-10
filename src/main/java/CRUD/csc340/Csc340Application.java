795c891aeea35a39872f1c82b4627f17b72cbab3
yera888 <yeraldinet1234@gmail.com>
2025-11-09 22:54:35 -0500

    Initial Commit

---
+++ src/main/java/CRUD/csc340/Csc340Application.java
@@ -0,0 +1,13 @@
+package CRUD.csc340;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc340Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csc340Application.class, args);
+	}
+
+}
---
+++ src/test/java/CRUD/csc340/Csc340ApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUD.csc340;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc340ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
