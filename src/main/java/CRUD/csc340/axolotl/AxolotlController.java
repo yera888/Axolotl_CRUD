efe97fb389e7175a77494557e21403f62ffe516e
yera888 <yeraldinet1234@gmail.com>
2025-11-10 21:01:23 -0500

    Add import for PathVariable in AxolotlController

--- src/main/java/CRUD/csc340/axolotl/AxolotlController.java
+++ src/main/java/CRUD/csc340/axolotl/AxolotlController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity; // used for parity with demo helpers
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
