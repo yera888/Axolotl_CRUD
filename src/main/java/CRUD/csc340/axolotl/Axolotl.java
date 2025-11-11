422267b8cf5202ca2b2e7c123c67ee7ea7bb46f5
yera888 <yeraldinet1234@gmail.com>
2025-11-10 20:47:43 -0500

    Refactor Axolotl class and remove validation annotations

--- src/main/java/CRUD/csc340/axolotl/Axolotl.java
+++ src/main/java/CRUD/csc340/axolotl/Axolotl.java
@@ -1,51 +1,47 @@
 package CRUD.csc340.axolotl;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "axolotls")
 public class Axolotl {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @NotBlank
-    @Column(nullable = false)
-    private String name;
+  @Column(nullable = false)
+  private String name;
 
-    @NotBlank
-    @Column(nullable = false)
-    private String morph;    
+  @Column(nullable = false)
+  private String morph; 
+  private String sex;     
 
-    private String sex;    
+  @Column(columnDefinition = "TEXT", nullable = false)
+  private String description;
 
-    @Column(columnDefinition = "TEXT", nullable = false)
-    private String description;
+  public Axolotl() {}
 
-    public Axolotl() {}
+  public Axolotl(Long id, String name, String morph, String sex, String description) {
+    this.id = id;
+    this.name = name;
+    this.morph = morph;
+    this.sex = sex;
+    this.description = description;
+  }
 
-    public Axolotl(Long id, String name, String morph, String sex, String description) {
-        this.id = id;
-        this.name = name;
-        this.morph = morph;
-        this.sex = sex;
-        this.description = description;
-    }
+  public Long getId() { return id; }
+  public void setId(Long id) { this.id = id; }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+  public String getMorph() { return morph; }
+  public void setMorph(String morph) { this.morph = morph; }
 
-    public String getMorph() { return morph; }
-    public void setMorph(String morph) { this.morph = morph; }
+  public String getSex() { return sex; }
+  public void setSex(String sex) { this.sex = sex; }
 
-    public String getSex() { return sex; }
-    public void setSex(String sex) { this.sex = sex; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+  public String getDescription() { return description; }
+  public void setDescription(String description) { this.description = description; }
 }
