ee76c0d31a06eda831bd422bad75899c80063b35
yera888 <yeraldinet1234@gmail.com>
2025-11-10 20:36:16 -0500

    Making changes to java mistake

--- src/main/java/CRUD/csc340/axolotl/AxolotlService.java
+++ src/main/java/CRUD/csc340/axolotl/AxolotlService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class AxolotlService {
 
     private final AxolotlRepository repo;
